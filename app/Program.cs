d3924db2f561c5c70dacd4b67f0168f52243b699
Rune Mydske Nielsen <rune.mydske.nielsen@gyldendal.no>
2018-11-20 19:14:50 +0100

    Initial

---
+++ app/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using yrlib;
+using System.Net.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace app
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var services = new ServiceCollection();
+      services.AddHttpClient();
+      services.AddTransient<Yrlib>();
+
+      var provider = services.BuildServiceProvider();
+      var yrlib = provider.GetService<Yrlib>();
+
+      Console.WriteLine(yrlib.GetForecast());
+    }
+  }
+}
---
+++ yrlib/Yrlib.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+
+namespace yrlib
+{
+  public class Yrlib
+  {
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public Yrlib(IHttpClientFactory httpClientFactory)
+    {
+      _httpClientFactory = httpClientFactory;
+    }
+    public string GetForecast()
+    {
+      var result = GetFromYr().Result;
+      return result;
+    }
+    public async Task<string> GetFromYr()
+    {
+      string uri = "http://www.yr.no/sted/Sverige/V%C3%A4stra_G%C3%B6taland/Skalhamn/varsel.xml";
+      // uri = "http://vind.template.no";
+      var client = _httpClientFactory.CreateClient();
+      var result = await client.GetStringAsync(uri);
+      return result;
+    }
+  }
+}
